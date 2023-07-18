d2592c4ff471b202dba06a346a5cd08388ab8a16
arsp <arspprashanthan@gmail.com>
2023-07-18 15:56:37 +0530

    clean the code

--- order-service/order-domain/order-domain-core/src/main/java/com/online/ordering/system/order/service/domain/event/OrderPaidEvent.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/online/ordering/system/order/service/domain/event/OrderPaidEvent.java
@@ -1,6 +1,5 @@
 package com.online.ordering.system.order.service.domain.event;
 
-import com.online.ordering.system.domain.event.DomainEvent;
 import com.online.ordering.system.order.service.domain.entity.Order;
 
 import java.time.ZonedDateTime;
